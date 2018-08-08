652fa717163160ebd2515de5b428c6968917ca8e
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-08-08 19:23:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 $(document).ready(function (){
 });
+
+function searchRepositories() {
+  let search = document.getElementById('searchTerms').value
+  $.get(`https://api.github.com/search/repositories?q=${search}`, function(data) {
+    console.log(data.items)
+    let repositoryTemplate = Handlebars.compile(document.getElementById('repository-template').innerHTML)
+    let repo = repositoryTemplate(data.items)
+    $('#results').html(repo)
+  });
+}
